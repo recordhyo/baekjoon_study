68762ce931d4774ba3098c703de3e397b21fa56c
recordhyo <126046238+recordhyo@users.noreply.github.com>
2023-03-01 14:55:53 +0900

    Add files via upload

---
+++ 3/11022.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void){
+
+    int a, b, n, i;
+    scanf("%d", &n);
+    for (i=0;i<n;i++){
+        scanf("%d %d",&a,&b);
+        printf("Case #%d: %d + %d = %d\n",(i+1),a,b,(a+b));
+    }
+
+    return 0;
+}
